91ca647203ef719336a9afb4c7f216c2cc01651a
Dian VK <diankinanev@abv.bg>
2022-11-08 13:32:20 +0200

    Create data_types.py

---
+++ Functions - More Exercises/data_types.py
@@ -0,0 +1,14 @@
+def check_data_type(inp_type,el):
+    result = ""
+    if inp_type == "int":
+        result = f"{int(el) * 2:.0f}"
+    elif inp_type == "real":
+        result = f"{float(el) * 1.5:.2f}"
+    elif inp_type == "string":
+        result = "$" + el + "$"
+    return result
+
+
+data_type = input()
+element = input()
+print(check_data_type(data_type, element))
