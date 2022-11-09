895c6261c6d65251120b417c4f264b71510efdf2
Dian VK <diankinanev@abv.bg>
2022-11-09 09:32:39 +0200

    Update

--- Lists Advanced - Lab/no_vowels.py
+++ Lists Advanced - Lab/no_vowels.py
@@ -1,3 +1,4 @@
-list_of_items = (item for item in range(len(input())):
-
-print(list_of_items)
+list_of_items = input()
+list_to_remove = "aoueiAOUEI"
+result = [letter for letter in list_of_items if letter not in list_to_remove]
+print("".join(result))
