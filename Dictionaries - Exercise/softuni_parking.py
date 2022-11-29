5bda16871ad5c4d9cd4e3ed892beb6a922a31b01
Dian VK <diankinanev@abv.bg>
2022-11-29 18:35:16 +0200

    Update

--- Dictionaries - Exercise/softuni_parking.py
+++ Dictionaries - Exercise/softuni_parking.py
@@ -9,8 +9,18 @@ for person in range(count_people):
         car_number = action_info[2]
         if person_name not in database:
             database[person_name] = car_number
-
+            print(f"{person_name} registered {car_number} successfully")
+        else:
+            print(f"ERROR: already registered with plate number {car_number}")
 
     elif action == "unregister":
         person_name = action_info[1]
-        pass
+        if person_name not in database:
+            print(f"ERROR: user {person_name} not found")
+        else:
+            print(f"{person_name} unregistered successfully")
+            del(database[person_name])
+
+
+for user,car_num in database.items():
+    print(f"{user} => {car_num}")
