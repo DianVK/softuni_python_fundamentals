0ba3d4f86c5d64bee721f3e7ec79d5cd4809b597
Dian VK <diankinanev@abv.bg>
2022-11-30 11:03:25 +0200

    Update

--- Dictionaries - Exercise/courses.py
+++ Dictionaries - Exercise/courses.py
@@ -1,14 +1,18 @@
-courses = input()
-courses_info = {}
+courses = {}
+command = input()
+while command != "end":
+    course_name,student_name = command.split(" : ")
+    if course_name not in courses:
+        courses[course_name] = []
+    courses[course_name].append(student_name)
 
-while courses != "end":
-    course_name, student_name = courses.split(" : ")
-    courses_info[course_name] = courses_info.get(course_name,  {})
-    courses_info[course_name][student_name] = student_name
+    command = input()
 
-    courses = input()
+for course in courses.items():
+    current_course = course[0]
+    current_students = course[1]
+    count_students_in_course = 0
+    print(f"{current_course}: {len(current_students)}")
+    for students in range(len(current_students)):
 
-for key, value in courses_info.items():
-    print(f"{key}: {len(courses_info[key])}")
-    for name in courses_info[key]:
-        print(f"-- {courses_info[key][name]}")
+        print(f"-- {current_students[students]}")
