3943e78e0fc17d35eb1fed3a4804414fef60e11d
Dian VK <diankinanev@abv.bg>
2022-12-02 10:44:43 +0200

    Update

--- Regular Expressions - Lab/match_full_name.py
+++ Regular Expressions - Lab/match_full_name.py
@@ -1,3 +1,6 @@
 import re
+names = input()
 
-pattern = r"([A-Z][a-z]+ [A-Z][a-z]+)"
+pattern = r"\b[A-Z][a-z]+[ ][A-Z][a-z]+\b"
+matching_names = re.findall(pattern, names)
+print(' '.join(matching_names))
