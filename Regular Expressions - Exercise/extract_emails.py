a762c122cd030acfd07c6d7f884b709b731412a3
Dian VK <diankinanev@abv.bg>
2022-12-06 21:51:41 +0200

    Update

--- Regular Expressions - Exercise/extract_emails.py
+++ Regular Expressions - Exercise/extract_emails.py
@@ -1,6 +1,6 @@
 import re
-check_email = input()
-
-pattern = r"(^|(?<=\s))[a-zA-Z0-9]+[\._-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+"
-correct_email = [obj.group() for obj in re.finditer(pattern, check_email)]
-print(*correct_email,sep="\n")
+text = input()
+mail_pattern = re.compile(r"(^|\s)(?P<valid_name>([a-z]+[\.\-]?)+[@][a-z]+([\-\.]?[a-z]+)?([\.][a-z]+)+)")
+valid_mail = mail_pattern.finditer(text)
+for valid in valid_mail:
+    print(f"{valid['valid_name']}")
