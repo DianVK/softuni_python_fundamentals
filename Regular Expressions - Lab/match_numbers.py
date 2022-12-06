897d0d936049c90957e1aee3c48bb7e80b0dc054
Dian VK <diankinanev@abv.bg>
2022-12-06 09:43:09 +0200

    Update

--- Regular Expressions - Lab/match_numbers.py
+++ Regular Expressions - Lab/match_numbers.py
@@ -1,17 +1,5 @@
 import re
-
-numbers = input()
-number_pattern = r'(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))'
-check_numbers = re.finditer(number_pattern,numbers)
-valid_numbers = []
-for valid in check_numbers:
-    valid_numbers.append(valid[0])
-
-print(*valid_numbers, sep=" ")
-
-# import re
-# text = input()
-#
-# pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
-# matches = [match.group() for match in re.finditer(pattern, text)]
-# print(*matches, sep=" ")
+digits = input()
+digit_pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
+valid_digits = [obj.group() for obj in re.finditer(digit_pattern,digits)]
+print(*valid_digits)
