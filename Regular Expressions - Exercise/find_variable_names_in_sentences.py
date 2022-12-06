723166d49d5ea5b6dfcc09e67c8a625b4f653225
Dian VK <diankinanev@abv.bg>
2022-12-06 13:02:55 +0200

    Update

--- Regular Expressions - Exercise/find_variable_names_in_sentences.py
+++ Regular Expressions - Exercise/find_variable_names_in_sentences.py
@@ -1,6 +1,9 @@
 import re
-text = input()
-searched_word = input()
-word_pattern = rf"\b{searched_word}\b"
-count_words = re.findall(word_pattern,text, re.IGNORECASE)
-print(len(count_words))
+sequence = input()
+text_pattern = re.compile(r"(^|(?<= ))_(?P<text>([A-Za-z]+))($|(?= ))")
+valid_pattern = text_pattern.finditer(sequence)
+texts = []
+for text in valid_pattern:
+    texts.append(text['text'])
+
+print(*texts,sep=",")
