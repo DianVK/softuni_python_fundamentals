b91ee1f23e28e79736d59e6ae6aeb14c4d350e37
Dian VK <diankinanev@abv.bg>
2022-12-06 21:18:56 +0200

    Update

--- Regular Expressions - Exercise/extract_the_links.py
+++ Regular Expressions - Exercise/extract_the_links.py
@@ -1,9 +1,8 @@
 import re
-
-pattern = re.compile(r'(?P<sub_domain>w{3}\.)(?P<domain>[A-Za-z0-9\-]+\.)(?P<extension>[a-z]+[\-.]*[a-z0-9\-.]*)')
-message = input()
-while len(message):
-    matches = pattern.finditer(message)
-    for match in matches:
-        print(f"{match['sub_domain']}{match['domain']}{match['extension']}")
-    message = input()
+links = input()
+link_pattern = re.compile(r"(?P<sub_domain>w{3}\.)(?P<domain>[A-Za-z0-9\-]+\.)(?P<extension>[a-z]+[\-.]*[a-z0-9\-.]*)")
+while len(links):
+    valid_link = link_pattern.finditer(links)
+    for valid in valid_link:
+        print(f"{valid['sub_domain']}{valid['domain']}{valid['extension']}")
+    links = input()
