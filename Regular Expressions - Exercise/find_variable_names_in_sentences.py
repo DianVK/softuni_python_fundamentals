139121f0ba7f4fd0d3959dbdecde6f8225a2d465
Dian VK <diankinanev@abv.bg>
2022-12-06 13:01:47 +0200

    Update

--- Regular Expressions - Exercise/find_variable_names_in_sentences.py
+++ Regular Expressions - Exercise/find_variable_names_in_sentences.py
@@ -2,5 +2,5 @@ import re
 text = input()
 searched_word = input()
 word_pattern = rf"\b{searched_word}\b"
-count_words = re.findall(searched_word,text, re.IGNORECASE)
+count_words = re.findall(word_pattern,text, re.IGNORECASE)
 print(len(count_words))
