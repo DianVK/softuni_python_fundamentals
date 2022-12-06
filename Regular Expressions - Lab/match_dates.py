a14c0ad566a799bbfd601510cb1a44e5cc5bdb0c
Dian VK <diankinanev@abv.bg>
2022-12-06 09:25:19 +0200

    Update

--- Regular Expressions - Lab/match_dates.py
+++ Regular Expressions - Lab/match_dates.py
@@ -1,31 +1,10 @@
 import re
-date = input()
-date_pattern = r"[\d]{2}([.|\-|//])[A-Za-z]{3}(\1)[\d]{4}"
-check_dates = re.finditer(date_pattern, date)
-valid_dates = []
-for valid in check_dates:
-    valid_dates.append(valid[0])
-
-for item in valid_dates:
-    spliter = item[2]
-    day,month,year = item.split(f"{spliter}")
-    print(f"Day: {day}, Month: {month}, Year: {year}")
-
-# import re
-# dates = input()
-#
-# correct_dates = r"([0-9][0-9][/][A-Z][a-z][a-z][/]\d{4}|[0-9][0-9][-][A-Z][a-z][a-z][-]\d{4}|[0-9][0-9][.][A-Z][a-z][a-z][.]\d{4})"
-#
-# matches = re.finditer(correct_dates, dates)
-# results = []
-# for match in matches:
-#     results.append(match[0])
-#
-# for el in range(len(results)):
-#     current_date = results[el]
-#     for x in range(len(current_date)):
-#         day = current_date[0] + current_date[1]
-#         month = current_date[3] + current_date[4] + current_date[5]
-#         year = current_date[7] + current_date[8] + current_date[9] + current_date[10]
-#         print(f"Day: {day}, Month: {month}, Year: {year}")
-#         break
+dates = input()
+date_pattern = r"\b\d{2}(?P<separator>[\.\-\/])[A-Za-z]{3}(?P=separator)\d{4}\b"
+valid_dates = [obj.group() for obj in re.finditer(date_pattern,dates)]
+for date in range(len(valid_dates)):
+    current_date = valid_dates[date]
+    current_day = current_date[0:2]
+    current_month = current_date[3:6]
+    current_year = current_date[7:11]
+    print(f"Day: {current_day}, Month: {current_month}, Year: {current_year}")
