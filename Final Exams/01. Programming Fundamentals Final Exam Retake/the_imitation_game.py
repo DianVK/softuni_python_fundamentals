126f7548f8f63ab00d15525f622b1ef61ea45379
Dian VK <diankinanev@abv.bg>
2022-12-07 10:49:40 +0200

    Update

---
+++ Final Exams/01. Programming Fundamentals Final Exam Retake/the_imitation_game.py
@@ -0,0 +1,36 @@
+encrypted_message = input()
+
+
+def main():
+    command = input()
+    while command != "Decode":
+        command = command.split("|")
+        action = command[0]
+        if action == "Move":
+            move(int(command[1]))
+        elif action == "Insert":
+            insert(int(command[1]),command[2])
+        elif action == "ChangeAll":
+            change_all(command[1],command[2])
+        command = input()
+    print(f"The decrypted message is: {encrypted_message}")
+
+
+def move(number_of_letters):
+    global encrypted_message
+    first_letters = encrypted_message[0:number_of_letters]
+    encrypted_message = encrypted_message[number_of_letters:] + first_letters
+
+
+def insert(index,value):
+    global encrypted_message
+    encrypted_message = encrypted_message[0:index] + value + encrypted_message[index:]
+
+
+def change_all(substring,replacement):
+    global encrypted_message
+    if substring in encrypted_message:
+        encrypted_message = encrypted_message.replace(substring,replacement)
+
+
+main()
