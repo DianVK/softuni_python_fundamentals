ff7e9bfc520f9ba5e82822495088a5690ab117e3
Dian VK <diankinanev@abv.bg>
2022-12-09 10:26:03 +0200

    Update

--- Final Exams/05. Programming Fundamentals Final Exam/emoji_detector.py
+++ Final Exams/05. Programming Fundamentals Final Exam/emoji_detector.py
@@ -1,28 +1,14 @@
 import re
 text = input()
-text_pattern = re.compile(r'(^|(?<= ))(?P<separator>[\:]{2}|[\*]{2})(?P<word>[A-Za-z]+)(?P=separator)')
-valid_text = text_pattern.finditer(text)
-all_valid_text = text_pattern.finditer(text)
-all_valid_texts = []
-cool_words = []
-for valid in all_valid_text:
-    current_text = valid['separator'] + valid['word'] + valid['separator']
-    all_valid_texts.append(current_text)
-
-for valid in valid_text:
-    current_text = valid['word']
-    current_sum = 0
-    min_amount = 1
-    count_emojis = 0
-    for digit in range(len(text)):
-        current_digit = text[digit]
-        if current_digit.isdigit():
-            min_amount *= int(current_digit)
-    for char in range(len(current_text)):
-        current_char = current_text[char]
-        if current_char.isdigit():
-            current_sum += int(current_char)
-    if current_sum >= min_amount:
-        print(f"Cool threshold: {min_amount}")
-        print(f"{count_emojis} emojis found in the text. The cool ones are:")
-        print(*all_valid_text,sep="\n")
+text_pattern = re.compile(r'(:{2}|\*{2})(?P<word>[A-Z][a-z]{2,})\1')
+valid_texts = list(text_pattern.finditer(text))
+threshold = 1
+for character in text:
+    if character.isdigit():
+        threshold *= int(character)
+print(f"Cool threshold: {threshold}")
+print(f"{len(valid_texts)} emojis found in the text. The cool ones are:")
+for valid in valid_texts:
+    sum_of_ascii_letters = sum([ord(letter) for letter in valid['word']])
+    if sum_of_ascii_letters >= threshold:
+        print(valid[0])
